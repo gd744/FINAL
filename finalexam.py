97c3f576f5de57e94251dd30203d757954b64b3c
gd744 <gonzalezdavid7761@gmail.com>
2021-05-13 15:43:22 -0700

    structured-english

---
+++ finalexam.py
@@ -0,0 +1,6 @@
+"""
+This program will presennt # of grades, average of grades, and % of grades above the average.
+The raw grade data will be stripped from "Final.txt"
+separate calculations will be done for counting the number of grades, average, and exceptional scores.
+results will be printed to end user
+"""
