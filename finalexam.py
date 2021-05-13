e2cb2816e8019afba40d2c76d0c84e8db77fd3cc
gd744 <gonzalezdavid7761@gmail.com>
2021-05-13 15:51:53 -0700

    psuedocode

--- finalexam.py
+++ finalexam.py
@@ -4,3 +4,18 @@ The raw grade data will be stripped from "Final.txt"
 separate calculations will be done for counting the number of grades, average, and exceptional scores.
 results will be printed to end user
 """
+"""
+def strip_list()
+path final.txt
+open path to filename
+    scores = stripped scores from Final.txt
+    return the list to buffer
+def main 
+scores = strip_list to call on previous func
+print ("student scores are")
+average = sum(scores) / len(scores)
+for i in scores:
+    if float is greater than avg:
+        add 1
+        percent average = counter / len(scores)
+"""
